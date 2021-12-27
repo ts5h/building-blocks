399f25de85bc763d1482f42e83ea765c8a91a856
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-27 21:38:25 +0100

    Update some documents

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from '../App'
 
+// *** The following test does not work currently ***
 test('renders learn react link', () => {
   render(<App />)
   const linkElement = screen.getByText(/learn react/i)
