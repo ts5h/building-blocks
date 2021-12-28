2ee100085c743a87221df3da5230b9550bebd88b
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-28 01:38:28 +0100

    Update unit test (WIP)

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from '../App'
 
-// *** The following test does not work currently ***
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+// Firebase mock object
+jest.mock('firebase/compat/app', () => ({
+  initializeApp: () => ({
+    firestore: () => ({
+      collection: () => ({
+        doc: () => ({
+          onSnapshot: jest.fn().mockReturnThis(),
+        }),
+      }),
+    }),
+    auth: jest.fn(),
+  }),
+}))
+
+describe('App has playground and blocks', () => {
+  test('App contains #playground', () => {
+    render(<App />)
+    const playground = document.querySelector('#playground')
+    expect(playground).toBeInTheDocument()
+  })
+
+  test('App contains 100 blocks', () => {
+    render(<App />)
+    const blocks = document.querySelectorAll('.block')
+    expect(blocks.length).toBe(100)
+  })
 })
